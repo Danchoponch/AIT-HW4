288267f476715eede24cdbbe69fe95e135d075e6
Daniar <dz2307@nyu.edu>
2024-10-11 01:19:20 -0400

    3rd commit - part 2 finished

--- app.mjs
+++ app.mjs
@@ -3,15 +3,30 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Get the directory name
+// directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Initialize the Express application
+//init express
 const app = express();
 
-// Middleware to serve static files from the 'public' directory
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'public', 'views'));
+// console.log('Views folder:', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.redirect('/editor'); // Redirects to the editor page
+  });
+  
+app.get('/editor', (req, res) => {
+  res.render('editor'); // Renders editor.hbs template
+});
+
+app.get('/phonebook', (req, res) => {
+  res.render('phonebook'); // Renders phonebook.hbs template
+});
 
 // Set the port to 3000
 const PORT = 3000;
